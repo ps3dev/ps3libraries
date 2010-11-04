c44c79c686f42f4d8d3998fc184582130e16ee62
Dan Peori <dan.peori@oopo.net>
2010-11-04 14:26:59 -0300

    Forgot to remove fontconfig. Fixed.

---
+++ scripts/006-cairo-1.10.0.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# cairo-1.10.0.sh by Dan Peori (danpeori@oopo.net)
+
+## Download the source code.
+wget --continue http://cairographics.org/releases/cairo-1.10.0.tar.gz || { exit 1; }
+
+## Unpack the source code.
+rm -Rf cairo-1.10.0 && tar xfvz cairo-1.10.0.tar.gz && cd cairo-1.10.0 || { exit 1; }
+
+## Patch the source code.
+cat ../../patches/cairo-1.10.0-PPU.patch | patch -p1 || { exit 1; }
+
+## Create the build directory.
+mkdir build-ppu && cd build-ppu || { exit 1; }
+
+## Configure the build.
+CFLAGS="-O2 -Wall -L$PS3DEV/ppu/lib -DCAIRO_NO_MUTEX" CPPFLAGS="-I$PS3DEV/ppu/include" PKG_CONFIG_PATH="$PS3DEV/ppu/lib/pkgconfig" ../configure --prefix="$PS3DEV/ppu" --host="ppu" --enable-fc="no" --enable-xlib="no" --disable-shared || { exit 1; }
+
+## Compile and install.
+make -j4 && make install || { exit 1; }
