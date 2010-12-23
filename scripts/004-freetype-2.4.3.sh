421596a918d33d661bd19185b41ea04f9b4c1caf
Dan Peori <dan.peori@oopo.net>
2010-12-22 21:33:14 -0400

    Updated url, as suggested by Matt_P and DarkHPS3.

--- scripts/004-freetype-2.4.3.sh
+++ scripts/004-freetype-2.4.3.sh
@@ -2,7 +2,7 @@
 # freetype-2.4.3.sh by Dan Peori (danpeori@oopo.net)
 
 ## Download the source code.
-wget --continue http://download.savannah.gnu.org/releases/freetype/freetype-2.4.3.tar.bz2 || { exit 1; }
+wget --continue http://voxel.dl.sourceforge.net/project/freetype/freetype2/2.4.3/freetype-2.4.3.tar.gz || { exit 1; }
 
 ## Unpack the source code.
 rm -Rf freetype-2.4.3 && tar xfvj freetype-2.4.3.tar.bz2 && cd freetype-2.4.3 || { exit 1; }
