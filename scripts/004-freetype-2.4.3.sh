a1ca3a4925654142816f993f067e8581da5201b7
Dan Peori <dan.peori@oopo.net>
2011-04-11 23:27:52 -0300

    Fixed a bug in the freetype script.

--- scripts/004-freetype-2.4.3.sh
+++ scripts/004-freetype-2.4.3.sh
@@ -17,7 +17,7 @@ mkdir build-ppu && cd build-ppu || { exit 1; }
 CFLAGS="-I$PSL1GHT/ppu/include -I$PS3DEV/portlibs/ppu/include" \
 LDFLAGS="-L$PSL1GHT/ppu/lib -L$PS3DEV/portlibs/ppu/lib -lrt -llv2" \
 PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
-GNUMAKE=${MAKE:-make} ../configure --prefix="$PS3DEV/host/ppu" --host="ppu" --disable-shared || { exit 1; }
+GNUMAKE=${MAKE:-make} ../configure --prefix="$PS3DEV/portlibs/ppu" --host="ppu" --disable-shared || { exit 1; }
 
 ## Compile and install.
 ${MAKE:-make} -j4 && ${MAKE:-make} install || { exit 1; }
