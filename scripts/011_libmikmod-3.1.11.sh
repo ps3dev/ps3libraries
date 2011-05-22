97a93c74ecfdd2b2efec8e3982c9d8f906f9b8a8
Marcus Comstedt <marcus@mc.pp.se>
2011-05-22 20:51:28 +0200

    Add libmikmod

---
+++ scripts/011_libmikmod-3.1.11.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# libmikmod-3.1.11.sh by Marcus Comstedt <marcus@mc.pp.se>
+
+## Download the source code.
+wget --continue http://mikmod.raphnet.net/files/libmikmod-3.1.11.tar.gz || { exit 1; }
+
+## Unpack the source code.
+rm -Rf libmikmod-3.1.11 && tar xfvz ./libmikmod-3.1.11.tar.gz && cd libmikmod-3.1.11 || { exit 1; }
+
+## Patch the source code.
+cat ../../patches/libmikmod-3.1.11-PPU.patch | patch -p1 || { exit 1; }
+
+## Create the build directory.
+mkdir build-ppu && cd build-ppu || { exit 1; }
+
+## Configure the build.
+CFLAGS="-I$PSL1GHT/ppu/include -I$PS3DEV/portlibs/ppu/include" \
+LDFLAGS="-L$PSL1GHT/ppu/lib -L$PS3DEV/portlibs/ppu/lib -lrt -llv2" \
+CC=ppu-gcc LD=ppu-ld NM=ppu-nm RANLIB=ppu-ranlib STRIP=ppu-strip \
+    ../configure --prefix="$PS3DEV/portlibs/ppu" --host=ppu \
+	--disable-esd --disable-dl --disable-shared \
+	|| { exit 1; }
+
+## Compile and install.
+${MAKE:-make} && ${MAKE:-make} install || { exit 1; }
