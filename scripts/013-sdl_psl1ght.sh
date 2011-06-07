b381ea2118fb10c031cfdf61b8687aa30c10d4be
Dan Peori <dan.peori@oopo.net>
2011-06-07 16:32:35 -0300

    Added SDL and SDL libraries from Zeldin's forks.

---
+++ scripts/013-sdl_psl1ght.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+# sdl_psl1ght.sh by Dan Peori (dan.peori@oopo.net)
+
+## Download the source code.
+wget --no-check-certificate https://github.com/zeldin/SDL_PSL1GHT/tarball/master -O sdl_psl1ght.tar.gz
+
+## Unpack the source code.
+rm -Rf sdl_psl1ght && mkdir sdl_psl1ght && tar --strip-components=1 --directory=sdl_psl1ght -xvzf sdl_psl1ght.tar.gz
+
+## Create the build directory.
+cd sdl_psl1ght
+
+## Compile and install.
+./script.sh && ${MAKE:-make} && ${MAKE:-make} install
---
+++ scripts/014-sdl_psl1ght_libs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+# sdl_psl1ght_libs.sh by Dan Peori (dan.peori@oopo.net)
+
+## Download the source code.
+wget --no-check-certificate https://github.com/zeldin/SDL_PSL1GHT_Libs/tarball/master -O sdl_psl1ght_libs.tar.gz
+
+## Unpack the source code.
+rm -Rf sdl_psl1ght_libs && mkdir sdl_psl1ght_libs && tar --strip-components=1 --directory=sdl_psl1ght_libs -xvzf sdl_psl1ght_libs.tar.gz
+
+## Create the build directory.
+cd sdl_psl1ght_libs
+
+## Compile and install.
+./make_SDL_Libs.sh
