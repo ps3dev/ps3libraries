5addd21cc3c72811d05eeb15c57eea18f5e6995f
dhewg <dhewg@wiibrew.org>
2011-07-06 18:45:15 +0200

    Use gmake for freetype if available
    Freetype hates BSD's make

--- scripts/004-freetype-2.4.3.sh
+++ scripts/004-freetype-2.4.3.sh
@@ -10,11 +10,14 @@ rm -Rf freetype-2.4.3 && tar xfvz freetype-2.4.3.tar.gz && cd freetype-2.4.3
 ## Create the build directory.
 mkdir build-ppu && cd build-ppu
 
+## freetype insists on GNU make
+which gmake 1>/dev/null 2>&1 && MAKE=gmake
+
 ## Configure the build.
 CFLAGS="-I$PSL1GHT/ppu/include -I$PS3DEV/portlibs/ppu/include" \
 LDFLAGS="-L$PSL1GHT/ppu/lib -L$PS3DEV/portlibs/ppu/lib -lrt -llv2" \
 PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
-GNUMAKE=${MAKE:-make} ../configure --prefix="$PS3DEV/portlibs/ppu" --host="powerpc64-ps3-elf" --disable-shared
+GNUMAKE=$MAKE ../configure --prefix="$PS3DEV/portlibs/ppu" --host="powerpc64-ps3-elf" --disable-shared
 
 ## Compile and install.
 ${MAKE:-make} -j4 && ${MAKE:-make} install
