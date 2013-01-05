f54fc736f4cf0dcd8c31d6abb7f2387d57f4e91e
Marcus Comstedt <marcus@mc.pp.se>
2013-01-05 19:52:21 +0100

    Fix zlib build error on MacOSX
    Thanks to AndoniZubimendi for finding the problem.
    
    Also, changed all remaining references to "1.2.6" into "1.2.7"...

--- scripts/001-zlib-1.2.7.sh
+++ scripts/001-zlib-1.2.7.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -e
-# zlib-1.2.6.sh by Dan Peori (danpeori@oopo.net)
+# zlib-1.2.7.sh by Dan Peori (danpeori@oopo.net)
 
 ## Download the source code.
 wget --continue http://zlib.net/zlib-1.2.7.tar.gz
 
 ## Unpack the source code.
-rm -Rf zlib-1.2.6 && tar xfvz zlib-1.2.7.tar.gz && cd zlib-1.2.7
+rm -Rf zlib-1.2.7 && tar xfvz zlib-1.2.7.tar.gz && cd zlib-1.2.7
 
 ## Patch the source code.
-cat ../../patches/zlib-1.2.6-PPU.patch | patch -p1
+cat ../../patches/zlib-1.2.7-PPU.patch | patch -p1
 
 ## Configure the build.
 AR="powerpc64-ps3-elf-ar" CC="powerpc64-ps3-elf-gcc" RANLIB="powerpc64-ps3-elf-ranlib" \
