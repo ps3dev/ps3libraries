30341251d82e30c606b6154ac8af0ffc4b49d93d
Darayus Nanavati <darayus.contact@gmail.com>
2013-04-16 19:17:09 +1000

    Fixed sourceforge download link

--- scripts/004-freetype-2.4.3.sh
+++ scripts/004-freetype-2.4.3.sh
@@ -2,7 +2,7 @@
 # freetype-2.4.3.sh by Dan Peori (danpeori@oopo.net)
 
 ## Download the source code.
-wget --continue http://voxel.dl.sourceforge.net/project/freetype/freetype2/2.4.3/freetype-2.4.3.tar.gz
+wget --continue http://download.savannah.gnu.org/releases/freetype/freetype-2.4.3.tar.gz
 
 ## Unpack the source code.
 rm -Rf freetype-2.4.3 && tar xfvz freetype-2.4.3.tar.gz && cd freetype-2.4.3
