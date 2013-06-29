8958b6630b0dbdcb2af117b7540943b9bef6097a
Mohammad Haseeb <mhaqsb@yahoo.com>
2013-06-30 00:39:47 +0500

    Create 015-polarssl.sh
    PolarSSL compile script for ps3libraries.

---
+++ scripts/015-polarssl.sh
@@ -0,0 +1,75 @@
+#!/bin/sh -e
+
+# Automatic build script for polarssl
+# for psl1ght, playstaion 3 open source sdk.
+#
+# Originally Created by Felix Schulze on 08.04.11.
+# Ported to PS3Libraries to compile with psl1ght.
+# Copyright 2010 Felix Schulze. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+###########################################################################
+# Change values here
+#
+VERSION="1.2.5"
+#
+###########################################################################
+#
+# Don't change anything here
+CURRENTPATH=`pwd`
+ARCH="powerpc64"
+PLATFORM="APPLE"
+
+## Download the source code.
+wget --continue https://polarssl.org/download/polarssl-${VERSION}-gpl.tgz
+
+## Unpack the source code.
+rm -Rf polarssl-${VERSION} && tar xfvz polarssl-${VERSION}.gpl.tgz && cd polarssl-${VERSION}/library
+
+## Patch the source code.
+echo "Patching net.c and timing.c for compatibility..."
+cat ../../../patches/polarssl-1.2.5-net.patch | patch -p1
+cat ../../../patches/polarssl-1.2.5-timing.patch | patch -p1
+
+## Create the build directory.
+mkdir build-ppu && cd build-ppu
+
+echo "Building polarssl for ${PLATFORM} ${SDKVERSION} ${ARCH}"
+
+echo "Patching Makefile..."
+sed -i.bak '4d' ${CURRENTPATH}/polarssl-${VERSION}/library/Makefile
+
+echo "Please stand by..."
+
+export CC=$PS3DEV/ppu/bin/powerpc64-ps3-elf-gcc
+export LD=$PS3DEV/ppu/bin/powerpc64-ps3-elf-ld
+export CPP=$PS3DEV/ppu/bin/powerpc64-ps3-elf-cpp
+export CXX=$PS3DEV/ppu/bin/powerpc64-ps3-elf-g++
+export AR=$PS3DEV/ppu/bin/powerpc64-ps3-elf-ar
+export AS=$PS3DEV/ppu/bin/powerpc64-ps3-elf-as
+export NM=$PS3DEV/ppu/bin/powerpc64-ps3-elf-nm
+export CXXCPP=$PS3DEV/ppu/bin/powerpc64-ps3-elf-cpp
+export RANLIB=$PS3DEV/ppu/bin/powerpc64-ps3-elf-ranlib
+export LDFLAGS="-L$PS3DEV/ppu/powerpc64-ps3-elf/lib -L$PSL1GHT/ppu/lib -L$PS3DEV/portlibs/ppu/lib -lrt -llv2"
+export CFLAGS="-I${CURRENTPATH}/polarssl-${VERSION}/include -I$PS3DEV/ppu/powerpc64-ps3-elf/include -I$PSL1GHT/ppu/include -I$PS3DEV/portlibs/ppu/include -mcpu=cell"
+
+echo "Build library..."
+## Compile and install.
+${MAKE:-make}
+
+cp libpolarssl.a $PS3DEV/portlibs/ppu/lib/libpolarssl.a
+cp -R ${CURRENTPATH}/polarssl-${VERSION}/include $PS3DEV/portlibs/ppu/include/
+cp ${CURRENTPATH}/polarssl-${VERSION}/LICENSE $PS3DEV/portlibs/ppu/include/polarssl/LICENSE
+
+echo "Building done."
