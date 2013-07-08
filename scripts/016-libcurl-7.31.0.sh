bc2de847dad3e79677aa2acdab6ea07256ef2a66
mhaqs <mhaqsb@yahoo.com>
2013-07-08 19:51:09 +0500

    new libs for psl1ght - proper - Updated libcurl to the latest version 7.31.0. - Ported polarssl 1.2.8 to psl1ght. - Reorganized ps3libraries script order to compile curl with polarssl support. SSL support is enabled in curl. - Ported lib json-c to psl1ght. - Updated patches for libcurl, polarssl for compilation. - Updated execution rights on new scripts. - zlib script execution fix introduced in last commit

--- scripts/016-libcurl-7.31.0.sh
+++ scripts/016-libcurl-7.31.0.sh
@@ -1,28 +1,28 @@
 #!/bin/sh -e
-# libcurl-7.21.6 by KaKaRoTo
+# libcurl-7.31.0 by KaKaRoTo
+# modified by mhaqs for 7.31.0 release and cpp compatibility
 
 ## Download the source code.
-wget --continue http://curl.haxx.se/download/curl-7.21.6.tar.gz
+wget --continue http://curl.haxx.se/download/curl-7.31.0.tar.gz
 
 ## Unpack the source code.
-rm -Rf curl-7.21.6 && tar xfvz curl-7.21.6.tar.gz && cd curl-7.21.6
+rm -Rf curl-7.31.0 && tar xfvz curl-7.31.0.tar.gz && cd curl-7.31.0
 
 ## Patch the source code.
-cat ../../patches/libcurl-7.21.6.patch | patch -p1
-
+cat ../../patches/libcurl-7.31.0.patch | patch -p1
 
 ## Create the build directory.
 mkdir build-ppu && cd build-ppu
 
 ## Configure the build.
 AR="ppu-ar" CC="ppu-gcc" RANLIB="ppu-ranlib" \
-  CFLAGS="-O2 -Wall -I$PSL1GHT/ppu/include -I$PS3DEV/portlibs/ppu/include -I$PSL1GHT/ppu/include/net" \
+  CFLAGS="-O2 -Wall" \
   CXXFLAGS="-I$PSL1GHT/ppu/include -I$PS3DEV/portlibs/ppu/include" \
   CPPFLAGS=" -I$PSL1GHT/ppu/include -I$PS3DEV/portlibs/ppu/include -I$PSL1GHT/ppu/include/net"  \
   LDFLAGS="-L$PSL1GHT/ppu/lib -L$PS3DEV/portlibs/ppu/lib" LIBS="-lnet -lsysutil -lsysmodule -lm " \
   PKG_CONFIG_LIBDIR="$PSL1GHT/ppu/lib/pkgconfig" PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
      ../configure   --prefix="$PS3DEV/portlibs/ppu"  --host="powerpc64-ps3-elf"  \
-          --includedir="$PS3DEV/portlibs/ppu/include"   --libdir="$PS3DEV/portlibs/ppu/lib"
+          --includedir="$PS3DEV/portlibs/ppu/include"   --libdir="$PS3DEV/portlibs/ppu/lib" --without-ssl --with-polarssl="$PS3DEV/portlibs/ppu/include/polarssl" --with-ca-bundle="/usr/ssl/certs/ca-bundle.crt"
 
 ## Compile and install.
 ${MAKE:-make} -j4 && ${MAKE:-make} install
