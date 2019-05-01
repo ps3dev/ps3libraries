a26a929c18de8deb1151d83d97e8774ca1bdcc98
Marcus Comstedt <marcus@mc.pp.se>
2019-05-01 15:55:12 +0200

    Use up-to-date config.guess and config.sub also for libcurl

--- scripts/016-libcurl-7.31.0.sh
+++ scripts/016-libcurl-7.31.0.sh
@@ -5,8 +5,15 @@
 ## Download the source code.
 wget --continue http://curl.haxx.se/download/curl-7.31.0.tar.gz
 
+## Download an up-to-date config.guess and config.sub
+if [ ! -f config.guess ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.guess; fi
+if [ ! -f config.sub ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.sub; fi
+
 ## Unpack the source code.
 rm -Rf curl-7.31.0 && tar xfvz curl-7.31.0.tar.gz && cd curl-7.31.0
+
+## Replace config.guess and config.sub
+cp ../config.guess ../config.sub .
 
 ## Patch the source code.
 cat ../../patches/libcurl-7.31.0.patch | patch -p1
