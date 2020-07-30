33eb1c85f884151fcbf4f5ffd1714637f342d10c
Damian <dparrino@gmail.com>
2020-07-29 22:22:27 -0300

    Update cURL to 7.64.1
    using polarSSL

--- scripts/018-libcurl-7.64.1.sh
+++ scripts/018-libcurl-7.64.1.sh
@@ -2,21 +2,19 @@
 # libcurl-7.31.0 by KaKaRoTo
 # modified by mhaqs for 7.31.0 release and cpp compatibility
 
+VERSION=7.64.1
 ## Download the source code.
-../download.sh curl-7.31.0.tar.gz
+../download.sh curl-${VERSION}.tar.gz
 
 ## Download an up-to-date config.guess and config.sub
 ../download.sh config.guess
 ../download.sh config.sub
 
 ## Unpack the source code.
-rm -Rf curl-7.31.0 && tar xfvz ../archives/curl-7.31.0.tar.gz && cd curl-7.31.0
+rm -Rf curl-${VERSION} && tar xfvz ../archives/curl-${VERSION}.tar.gz && cd curl-${VERSION}
 
 ## Replace config.guess and config.sub
 cp ../../archives/config.guess ../../archives/config.sub .
-
-## Patch the source code.
-cat ../../patches/libcurl-7.31.0.patch | patch -p1
 
 ## Create the build directory.
 mkdir build-ppu && cd build-ppu
@@ -29,6 +27,7 @@ AR="ppu-ar" CC="ppu-gcc" RANLIB="ppu-ranlib" \
   LDFLAGS="-L$PSL1GHT/ppu/lib -L$PS3DEV/portlibs/ppu/lib" LIBS="-lnet -lsysutil -lsysmodule -lm " \
   PKG_CONFIG_LIBDIR="$PSL1GHT/ppu/lib/pkgconfig" PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
      ../configure   --prefix="$PS3DEV/portlibs/ppu"  --host="powerpc64-ps3-elf"  \
+          --disable-threaded-resolver --disable-ipv6 \
           --includedir="$PS3DEV/portlibs/ppu/include"   --libdir="$PS3DEV/portlibs/ppu/lib" --without-ssl --with-polarssl="$PS3DEV/portlibs/ppu/include/polarssl" --with-ca-bundle="/usr/ssl/certs/ca-bundle.crt"
 
 ## Compile and install.
