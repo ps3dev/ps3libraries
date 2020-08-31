f17a1feeedec28383a36f77c900d79f1471f7ea6
Damian Parrino <dparrino@gmail.com>
2020-08-31 12:05:12 -0300

    Fix filename

--- scripts/030-libnfs.sh
+++ scripts/030-libnfs.sh
@@ -4,7 +4,7 @@
 #	ported to PS3 by Bucanero
 
 ## Download the source code.
-wget --no-check-certificate https://github.com/sahlberg/libnfs/tarball/master -O tiny3d.tar.gz 
+wget --no-check-certificate https://github.com/sahlberg/libnfs/tarball/master -O libnfs.tar.gz 
 
 ## Unpack the source code.
 rm -Rf libnfs && mkdir libnfs && tar --strip-components=1 --directory=libnfs -xvzf libnfs.tar.gz && cd libnfs
