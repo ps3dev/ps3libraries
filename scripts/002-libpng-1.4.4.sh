be834909ca4db26beab8e7af551765172df9540c
Salvo Cristaldi <scristaldi@cca.unict.it>
2021-01-29 11:34:27 +0100

    Fix #50

--- scripts/002-libpng-1.4.4.sh
+++ scripts/002-libpng-1.4.4.sh
@@ -19,6 +19,7 @@ mkdir build-ppu && cd build-ppu
 
 ## Configure the build.
 CFLAGS="-I$PSL1GHT/ppu/include -I$PS3DEV/portlibs/ppu/include" \
+CPPFLAGS="-I$PSL1GHT/ppu/include -I$PS3DEV/portlibs/ppu/include" \
 LDFLAGS="-L$PSL1GHT/ppu/lib -L$PS3DEV/portlibs/ppu/lib -lrt -llv2" \
 PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
 ../configure --prefix="$PS3DEV/portlibs/ppu" --host="powerpc64-ps3-elf" --enable-static --disable-shared
