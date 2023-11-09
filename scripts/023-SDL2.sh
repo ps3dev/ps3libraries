398f65420edfa9512d5fef0f69817596aafc39f1
StrawFox64 <122846274+StrawFox64@users.noreply.github.com>
2023-11-08 22:12:18 -0300

    Fixed file name

--- scripts/023-SDL2.sh
+++ scripts/023-SDL2.sh
@@ -2,7 +2,7 @@
 # sdl2.sh by Naomi Peori (naomi@peori.ca)
 
 ## Download the source code.
-wget --no-check-certificate https://github.com/StrawFox64/SDL2PSL1GHT/tarball/master -O psl1ght.tar.gz
+wget --no-check-certificate https://github.com/StrawFox64/SDL2PSL1GHT/tarball/master -O SDL2PSL1GHT.tar.gz
 
 ## Create the build directory.
 rm -Rf SDL2PSL1GHT && mkdir SDL2PSL1GHT
