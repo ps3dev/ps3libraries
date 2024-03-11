7e67cb04d129b9cab5279191b619fbd5e86b1e68
Humberto Dias <hldtux@gmail.com>
2024-03-11 01:05:20 -0300

    libxml2 --without-lzma

--- scripts/012-libxml2-2.9.12.sh
+++ scripts/012-libxml2-2.9.12.sh
@@ -24,7 +24,7 @@ mkdir build-ppu && cd build-ppu
 CFLAGS="-I$PSL1GHT/ppu/include -I$PS3DEV/portlibs/ppu/include" \
 LDFLAGS="-L$PSL1GHT/ppu/lib -L$PS3DEV/portlibs/ppu/lib -lrt -llv2" \
 PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
-../configure --prefix="$PS3DEV/portlibs/ppu" --host="powerpc64-ps3-elf" --enable-static --disable-shared --without-ftp --without-http --without-python
+../configure --prefix="$PS3DEV/portlibs/ppu" --host="powerpc64-ps3-elf" --enable-static --disable-shared --without-ftp --without-http --without-python --without-lzma
 
 ## Compile and install.
 ${MAKE:-make} -j4 && ${MAKE:-make} install
