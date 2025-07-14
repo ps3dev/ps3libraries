af4535e5c09e1a117b8e2bbaf2cdd2ca67d965d3
mdukat <mateusz.dukat1@gmail.com>
2025-07-15 00:54:53 +0200

    FIX: links for freetype and libxml2

--- scripts/012-libxml2-2.7.8.sh
+++ scripts/012-libxml2-2.7.8.sh
@@ -2,14 +2,14 @@
 # libxml2-2.7.8.sh by Naomi Peori (naomi@peori.ca)
 
 ## Download the source code.
-wget --continue http://xmlsoft.org/download/libxml2-2.7.8.tar.gz
+wget --continue https://download.gnome.org/sources/libxml2/2.7/libxml2-2.7.8.tar.xz
 
 ## Download an up-to-date config.guess and config.sub
 if [ ! -f config.guess ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.guess; fi
 if [ ! -f config.sub ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.sub; fi
 
 ## Unpack the source code.
-rm -Rf libxml2-2.7.8 && tar xfvz libxml2-2.7.8.tar.gz && cd libxml2-2.7.8
+rm -Rf libxml2-2.7.8 && tar xfvJ libxml2-2.7.8.tar.xz && cd libxml2-2.7.8
 
 ## Replace config.guess and config.sub
 cp ../config.guess ../config.sub .
