fcab48c1f58072b66f1f19005a3b3973681c34b1
Daniel Welch <dwelch.appleid@gmail.com>
2026-06-03 21:39:18 +0100

    fix zlib error 415, use v1.2.13

--- scripts/001-zlib-1.2.11.sh
+++ scripts/001-zlib-1.2.11.sh
@@ -1,14 +1,15 @@
 #!/bin/sh -e
 # zlib-1.2.11.sh by Naomi Peori (naomi@peori.ca)
+ZLIB="zlib-1.2.13"
 
 ## Download the source code.
-wget --continue https://www.zlib.net/fossils/zlib-1.2.11.tar.gz
+wget --continue https://github.com/madler/zlib/releases/download/v1.2.13/${ZLIB}.tar.gz
 
 ## Unpack the source code.
-rm -Rf zlib-1.2.11 && tar xfvz zlib-1.2.11.tar.gz && cd zlib-1.2.11
+rm -Rf ${ZLIB} && tar xfvz ${ZLIB}.tar.gz && cd ${ZLIB}
 
 ## Patch the source code.
-cat ../../patches/zlib-1.2.11-PPU.patch | patch -p1
+cat ../../patches/${ZLIB}-PPU.patch | patch -p1
 
 ## Configure the build.
 AR="powerpc64-ps3-elf-ar" CC="powerpc64-ps3-elf-gcc" RANLIB="powerpc64-ps3-elf-ranlib" \
